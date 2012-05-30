525d223df8f3997fc5ae51e2494e6691ef5940b8
Jesse Dearing <jesse.dearing@gmail.com>
2012-05-30 01:44:02 -0500

    Adds lots of Vim + Tmux goodness
    Adds vimux
    Updates tmux.conf
    Auto runs tmux

--- zsh-custom/plugins/tmux/tmux.plugin.zsh
+++ zsh-custom/plugins/tmux/tmux.plugin.zsh
@@ -1,8 +1,7 @@
 [[ $TERM == "screen-256color" ]] && return
-[[ $- != *i* ]] && return
 tmux_sock=0
 while [[ -a "/tmp/tmux.$tmux_sock" ]] do
   tmux_sock=$[$tmux_sock + 1]
 done
 export CURRENT_TMUX_SOCKET="/tmp/tmux.$tmux_sock"
-tmux -S $CURRENT_TMUX_SOCKET && rm $CURRENT_TMUX_SOCKET && exit
+tmux -S $CURRENT_TMUX_SOCKET && rm $CURRENT_TMUX_SOCKET && echo "Closing in 2 seconds..." && sleep 2 && exit
