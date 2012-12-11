246a2ce1a1c60ae49fcb355406b721beb2cbede8
Jesse Dearing <jesse.dearing@gmail.com>
2012-12-11 13:49:26 -0800

    Extra os x options and sets executable

--- misc_scripts/osx.sh
+++ misc_scripts/osx.sh
@@ -2,5 +2,11 @@
 
 echo "Setting OS Xcustomizations..."
 defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+defaults write NSGlobalDomain InitialKeyRepeat -int 18
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+chflags nohidden ~/Library
 sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
 echo "Done, now restart"
