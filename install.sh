2d8893cfd86759d072bdb02d78af5e3e8f5335bb
Jesse Dearing <jesse.dearing@gmail.com>
2016-10-30 10:55:18 -0700

    Using a shell script instead of a Rakefile

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+(
+cd $HOME/.dotfiles
+
+all_dir=`find $PWD -maxdepth 1 \! -iname 'README.md' \! -iname '.*' \! -iname 'powerline-fonts' \! -iname misc_scripts \! -iname 'install.sh' \! -iname 'global.gems'`
+
+for dir in $all_dir; do
+  if [ ! -e $HOME/.`basename $dir` ]; then
+    ln -s $dir $HOME/.`basename $dir`
+  else
+    echo $HOME/.`basename $dir` already exists
+  fi
+done
+)
