7b6e734e687690985cdcede496ca1fbe5c4a55cf
Jesse Dearing <jesse.dearing@gmail.com>
2018-03-29 13:50:06 -0700

    ✅  For my daily todos

--- bin/editor.sh
+++ bin/editor.sh
@@ -1,3 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-/usr/local/bin/nvim "$@"
+VIM=/usr/local/bin/nvim
+args=""
+
+while (( "$#" )); do
+	if [[ "$1" == "-daily" ]]; then
+		(
+			cd $HOME/daily
+			$VIM index.md
+		)
+		return
+	fi
+
+	args="$args $1"
+	shift
+done
+
+$VIM $args
