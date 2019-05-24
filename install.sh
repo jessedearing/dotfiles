cd629c5a355c1768c913d72f8432c9aebb2bab9b
Jesse Dearing <jesse.dearing@gmail.com>
2019-05-24 14:09:17 -0700

    Adding CoC settings to dotfiles

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 (
 cd $HOME/.dotfiles
 
-all_dir=`find $PWD -maxdepth 1 \! -iname 'pkg' \! -iname 'themes' \! -iname 'README.md' \! -iname '.*' \! -iname 'powerline-fonts' \! -iname misc_scripts \! -iname 'install.sh' \! -iname 'global.gems' \! -iname 'gpg-agent.conf'`
+all_dir=`find $PWD -maxdepth 1 \! -iname 'pkg' \! -iname 'themes' \! -iname 'README.md' \! -iname '.*' \! -iname 'powerline-fonts' \! -iname misc_scripts \! -iname 'install.sh' \! -iname 'global.gems' \! -iname 'gpg-agent.conf' \! -iname 'coc-settings.json'`
 
 for dir in $all_dir; do
   if [ ! -e $HOME/.`basename $dir` ]; then
@@ -15,10 +15,17 @@ for dir in $all_dir; do
 done
 
 mkdir -p $HOME/.gnupg
-if [ -L $HOME/.gnupg/gpg-agent.conf ]; then
+if [ -e $HOME/.gnupg/gpg-agent.conf ]; then
 	echo $HOME/.gnupg/gpg-agent.conf already exists
 else
 	echo Linking gpg-agent.conf to $HOME/.gnupg/gpg-agent.conf
 	ln -s $HOME/.dotfiles/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
+fi
+
+if [ -e $HOME/.config/nvim/coc-settings.json ]; then
+  echo "$HOME/.config/nvim/coc-settings.json already exists"
+else
+  echo "Linking $HOME/.dotfiles/coc-settings.json to $HOME/.config/nvim/coc-settings.json"
+  ln -s $HOME/.dotfiles/coc-settings.json $HOME/.config/nvim/coc-settings.json
 fi
 )
