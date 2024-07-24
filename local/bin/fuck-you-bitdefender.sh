78dc7475bb8088b00c36e8a34d5429dd8a385093
Jesse Dearing <code@me.jesse.dev>
2024-07-24 15:07:11 -0700

    chore: cleanup old scripts, add git-spice

--- local/bin/fuck-you-bitdefender.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for pid in `ps aux | grep Bitdefender | grep -v grep | awk '{ print $2; }'`; do
-	sudo renice 20 $pid
-done
--- local/bin/fuck-you.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for pid in `ps aux | grep "$*" | grep -v grep | awk '{ print $2; }'`; do
-  kill -TERM $pid
-done
