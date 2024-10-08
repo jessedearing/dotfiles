5ae2ea78898ec69028add353957e752ff2dad2f8
Jesse Dearing <code@me.jesse.dev>
2024-10-07 22:27:58 -0700

    Add autocmds with save and push for Pensieve

---
+++ config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,34 @@
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "/Users/jesse/Documents/pensieve/*.md" },
+	callback = function()
+		local Job = require("plenary.job")
+		local fileName = vim.fn.expand("%:t")
+		Job:new({
+			command = "git",
+			args = { "-C", "/Users/jesse/Documents/pensieve", "add", "-A", "." },
+			on_exit = function(_, code)
+				if code == 0 then
+					Job:new({
+						command = "git",
+						args = {
+							"-C",
+							"/Users/jesse/Documents/pensieve",
+							"commit",
+							"-q",
+							"-m",
+							fileName,
+						},
+						on_exit = function(_, code)
+							if code == 0 then
+								Job:new({
+									command = "git",
+									args = { "-C", "/Users/jesse/Documents/pensieve", "push" },
+								}):start()
+							end
+						end,
+					}):start()
+				end
+			end,
+		}):start()
+	end,
+})
