1523c19d0cf85123b54227dc9b6e6e6a3d07022b
Jesse Dearing <code@me.jesse.dev>
2024-10-25 12:19:55 -0700

    Disabling Copilot, enabling Prettier

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,12 +1,12 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		opts = function(_, opts)
-			opts.mapping = vim.tbl_extend("force", opts.mapping, {
-				["<CR>"] = vim.NIL,
-				["<Tab>"] = LazyVim.cmp.confirm({ select = false }),
-			})
-			opts.completion.completeopt = "menu,menuone,noinsert,noselect"
-		end,
-	},
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	opts = function(_, opts)
+	-- 		opts.mapping = vim.tbl_extend("force", opts.mapping, {
+	-- 			["<CR>"] = vim.NIL,
+	-- 			["<Tab>"] = LazyVim.cmp.confirm({ select = false }),
+	-- 		})
+	-- 		opts.completion.completeopt = "menu,menuone,noinsert,noselect"
+	-- 	end,
+	-- },
 }
