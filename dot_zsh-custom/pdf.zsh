1927e7671401717ff9fabc16b99e844ad22da185
Jesse Dearing <code@me.jesse.dev>
2024-12-30 21:48:42 -0800

    Add option to delete original

--- dot_zsh-custom/pdf.zsh
+++ dot_zsh-custom/pdf.zsh
@@ -1,3 +1,8 @@
 storedocument() {
   ocrmypdf "$1" "$HOME/Google Drive/My Drive/Scanned Documents/$1"
+  echo -n "Delete original $1?"
+  read __resp
+  if [[ "$__resp" =~ "^(y|yes)$" ]]; then
+    rm "$1"
+  fi
 }
