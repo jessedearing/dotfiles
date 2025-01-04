93fd33bcd3012b82107e33e3f32a2647b9bf9f4b
Jesse Dearing <code@me.jesse.dev>
2025-01-04 10:24:51 -0800

    Fix fzf in neovim

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -5,5 +5,10 @@ return {
 			{ "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
 			{ "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
 		},
+		opts = {
+			files = {
+				cmd = 'rg --files --no-ignore --hidden --follow -g "!{.git/**,node_modules/**,vendor/**}" 2> /dev/null',
+			},
+		},
 	},
 }
