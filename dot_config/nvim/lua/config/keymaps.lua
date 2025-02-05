0e52f74a8232f35fedd0e0bc612246790e71ec8e
Jesse Dearing <code@me.jesse.dev>
2025-02-05 11:55:32 -0800

    Lazyvim update

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,6 @@ local unmap = vim.keymap.del
 
 unmap("n", "<S-h>")
 unmap("n", "<S-l>")
-unmap("n", "<c-space>")
 
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move whole line down" })
 map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move whole line up" })
