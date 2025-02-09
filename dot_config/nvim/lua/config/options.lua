f2fd2d7e3d207c0c0490143d43086da7b82046d9
Jesse Dearing <code@me.jesse.dev>
2025-02-09 12:43:37 -0800

    LazyVim updates

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -7,3 +7,19 @@ vim.g.snips_author = "Jesse Dearing"
 opt.ruler = true
 opt.mouse = "a"
 opt.colorcolumn = "80"
+if vim.env.SSH_TTY then
+	local function paste()
+		return vim.split(vim.fn.getreg(""), "\n")
+	end
+	vim.g.clipboard = {
+		name = "OSC 52",
+		copy = {
+			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+		},
+		paste = {
+			["+"] = paste,
+			["*"] = paste,
+		},
+	}
+end
