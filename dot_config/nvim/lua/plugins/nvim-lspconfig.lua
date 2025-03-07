37f76b81076e066054ea028111b1f1116cef51ec
Jesse Dearing <code@me.jesse.dev>
2025-03-06 18:04:40 -0800

    Comment out openapi spec for now

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -18,7 +18,7 @@ return {
 								["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
 								["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
 								["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-								["https://spec.openapis.org/oas/3.1/schema/2024-11-14"] = "*api*.{yml,yaml}",
+								-- ["https://spec.openapis.org/oas/3.1/schema/2024-11-14"] = "*api*.{yml,yaml}",
 								["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
 								["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
 								kubernetes = {
