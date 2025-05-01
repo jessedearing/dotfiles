2df9a2cf1c54ff0aec65e1182e5e85884a4fc9bb
Jesse Dearing <code@me.jesse.dev>
2025-05-01 00:55:04 -0700

    Adding Go to Neovim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,14 @@
 local map = vim.keymap.set
 local unmap = vim.keymap.del
 
+local function goiferr()
+	require("go.iferr").run()
+end
+
 unmap("n", "<S-h>")
 unmap("n", "<S-l>")
 
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move whole line down" })
 map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move whole line up" })
+
+map("i", "<c-e>", goiferr, { desc = "Use iferr to automatically populate the if err block" })
