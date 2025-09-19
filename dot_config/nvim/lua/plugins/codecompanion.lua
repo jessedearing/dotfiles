6788a7d86d2a25881ee1b1f7ae896b3c2c68c2cb
Jesse Dearing <code@me.jesse.dev>
2025-09-19 06:49:06 -0700

    Comment out mcphub for right now

--- dot_config/nvim/lua/plugins/codecompanion.lua
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -28,16 +28,16 @@ return {
 					},
 				},
 			},
-			extensions = {
-				mcphub = {
-					callback = "mcphub.extensions.codecompanion",
-					opts = {
-						show_result_in_chat = true, -- Show mcp tool results in chat
-						make_vars = true, -- Convert resources to #variables
-						make_slash_commands = true, -- Add prompts as /slash commands
-					},
-				},
-			},
+			-- extensions = {
+			-- 	mcphub = {
+			-- 		callback = "mcphub.extensions.codecompanion",
+			-- 		opts = {
+			-- 			show_result_in_chat = true, -- Show mcp tool results in chat
+			-- 			make_vars = true, -- Convert resources to #variables
+			-- 			make_slash_commands = true, -- Add prompts as /slash commands
+			-- 		},
+			-- 	},
+			-- },
 		})
 	end,
 }
