d1c0e19808eb34b8453b8401deb5ab3c4ede9ce2
Jesse Dearing <code@me.jesse.dev>
2026-04-26 08:16:21 -0700

    Enable LazyVim plugins, but remove Obsidian

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
 		-- add LazyVim and import its plugins
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 		-- import/override with your plugins
-		-- { import = "plugins" },
+		{ import = "plugins" },
 	},
 	defaults = {
 		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
